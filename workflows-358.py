51155b7d343bff6e11c836cb7ededfc1b779256e
Bruno Grande <bruno.grande@sagebase.org>
2023-05-11 17:51:00 -0700

    Add synindex workflow step

--- workflows-358.py
+++ workflows-358.py
@@ -13,7 +13,7 @@ from utils import monitor_run
 async def main():
     ops = NextflowTowerOps()
     datasets = generate_datasets()
-    runs = [run_rnaseq_workflow(ops, dataset) for dataset in datasets]
+    runs = [run_workflows(ops, dataset) for dataset in datasets]
     statuses = await asyncio.gather(*runs)
     print(statuses)
 
@@ -22,10 +22,15 @@ async def main():
 class Dataset:
     id: str
     samplesheet: str
+    parent_id: str
 
     @property
-    def run_name(self):
+    def rnaseq_run_name(self):
         return f"rnaseq_{self.id}"
+
+    @property
+    def synindex_run_name(self):
+        return f"synindex_{self.id}"
 
 
 def generate_datasets() -> list[Dataset]:
@@ -35,23 +40,23 @@ def generate_datasets() -> list[Dataset]:
     """
     prefix = "s3://ntap-add5-project-tower-bucket/samplesheets/IBCDPE-528/synstage"
     return [
-        Dataset("syn51198953", f"{prefix}/syn51198953_RNA_Seq_SampleSheet.fixed.csv"),
-        Dataset("syn51198956", f"{prefix}/syn51198956_RNA_Seq_SampleSheet.csv"),
-        Dataset("syn51199003", f"{prefix}/syn51199003_RNA_Seq_SampleSheet.csv"),
-        Dataset("syn51199006", f"{prefix}/syn51199006_RNA_Seq_SampleSheet.csv"),
+        Dataset("syn51198953", f"{prefix}/syn51198953_RNA_Seq_SampleSheet.fixed.csv", ""),
+        Dataset("syn51198956", f"{prefix}/syn51198956_RNA_Seq_SampleSheet.csv", ""),
+        Dataset("syn51199003", f"{prefix}/syn51199003_RNA_Seq_SampleSheet.csv", ""),
+        Dataset("syn51199006", f"{prefix}/syn51199006_RNA_Seq_SampleSheet.csv", ""),
     ]
 
 
 def prepare_rnaseq_launch_info(dataset: Dataset) -> LaunchInfo:
     """Generate LaunchInfo for nf-core/rnaseq workflow run."""
     return LaunchInfo(
-        run_name=dataset.run_name,
+        run_name=dataset.rnaseq_run_name,
         pipeline="nf-core/rnaseq",
         revision="3.11.2",
         profiles=["sage"],
         params={
             "input": dataset.samplesheet,
-            "outdir": f"s3://ntap-add5-project-tower-bucket/outputs/{dataset.run_name}/",
+            "outdir": f"s3://ntap-add5-project-tower-bucket/outputs/{dataset.rnaseq_run_name}/",
             "gencode": True,
             "fasta": "s3://sage-igenomes/nf-core/rnaseq-3.7/grch38-gencode40/GRCh38.primary_assembly.genome.fa",
             "transcript_fasta": "s3://sage-igenomes/nf-core/rnaseq-3.7/grch38-gencode40/genome.transcripts.fa",
@@ -65,10 +70,28 @@ def prepare_rnaseq_launch_info(dataset: Dataset) -> LaunchInfo:
     )
 
 
-async def run_rnaseq_workflow(ops: NextflowTowerOps, dataset: Dataset):
+def prepare_synindex_launch_info(dataset: Dataset) -> LaunchInfo:
+    """Generate LaunchInfo for nf-synindex workflow run."""
+    return LaunchInfo(
+        run_name=dataset.synindex_run_name,
+        pipeline="Sage-Bionetworks-Workflows/nf-synindex",
+        revision="main",
+        profiles=["sage"],
+        params={
+            "s3_prefix": f"s3://ntap-add5-project-tower-bucket/outputs/{dataset.rnaseq_run_name}/",
+            "parent_id": dataset.parent_id,
+        },
+    )
+
+
+async def run_workflows(ops: NextflowTowerOps, dataset: Dataset):
     rnaseq_info = prepare_rnaseq_launch_info(dataset)
     rnaseq_run_id = ops.launch_workflow(rnaseq_info, "spot")
     await monitor_run(ops, rnaseq_run_id)
+
+    synindex_info = prepare_synindex_launch_info(dataset)
+    synindex_run_id = ops.launch_workflow(synindex_info, "spot")
+    await monitor_run(ops, synindex_run_id)
 
 
 if __name__ == "__main__":
